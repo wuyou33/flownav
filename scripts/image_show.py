4d64e433b58f2e7443cb68b97d57d4d4055c4a10
tdos <toemossgarcia@gmail.com>
2014-07-29 01:32:01 -0500

    Got template scaling working.

--- scripts/image_show.py
+++ scripts/image_show.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import copy
 import roslib
 roslib.load_manifest('flownav')
 import sys
@@ -49,74 +50,93 @@ def drawMatch(dm,kp1,kp2,dispim):
 def cvtIdx(pt,shape):
     return int(pt[1]*shape[1] + pt[0]) if hasattr(pt, '__len__') else map(int, pt%shape[1], pt//shape[1])
 
-def findAppoximateScaling(queryImg, trainImg, matches, queryDescs, trainDescs
-                          , queryKPs, trainKPs, compute):
+def findAppoximateScaling(queryImg, trainImg, matches, queryKPs, trainKPs, compute):
     trainTempImg = trainImg.copy()
-    # queryTempImg = queryImg.copy()    
-    scalerange = 1 + np.array(range(2,20,2))/10.
+    queryTempImg = queryImg.copy()    
+    scalerange = 1+np.array(range(10))/10.
     inttuple = lambda *x: tuple(map(int,x))
-
-    for m,querydesc,traindesc in zip(matches,queryDescs,trainDescs):
-        # get training keypoint location and size
-        x,y = queryKPs[m.queryIdx].pt
-        r = int(queryKPs[m.queryIdx].size//2)
+    roundtuple = lambda *x: tuple(map(lambda y: int(round(y)),x))
+    res = np.zeros(len(scalerange))
+    
+    for m in matches:
+        qkp = queryKPs[m.queryIdx]
+        tkp = trainKPs[m.trainIdx]        
 
         # extract the query image patch
-        x0,y0 = (x-r, y-r)
-        x1,y1 = (x+r, y+r)
+        x,y = qkp.pt
+        r = qkp.size*1.2*9/10
+
+        x0,y0 = roundtuple(x-r, y-r)
+        x1,y1 = roundtuple(x+r, y+r)
         querypatch = queryImg[y0:y1+1, x0:x1+1,:]
-        cv2.rectangle(trainTempImg, inttuple(x-r,y-r), inttuple(x+r,y+r), (0,0,255), 2)
-        
-        # trainpatch = trainImg[y0:y1+1, x0:x1+1,:]
-        # traindesc = compute(trainPatch)
+        cv2.rectangle(queryTempImg, (x0,y0), (x1,y1), (255,0,0), 2)
 
-        # template match patch on training image
-        try:
-            res = np.array(len(scalerange)+1)
-            res[0] = np.sum((querydesc-traindesc)**2)
-            for i,scale in enumerate(scalerange):
-                scaledtempl = cv2.resize(querypatch,0,fx=scale,fy=scale)
-                scaledesc = compute(scaledtempl,kp2)
-                res[i+1] = np.sum((scaledesc-traindesc)**2)
-                print res[i+1]
+        ckp = copy.copy(qkp)
+        ckp.pt = (qkp.pt[0]-x0, qkp.pt[1]-y0)
+        dispim = cv2.drawKeypoints(querypatch,[ckp], None, color=-1)
+        cv2.imshow('Template', dispim)
+        print "Template shape:", querypatch.shape
 
-            # res = [np.sum((compute(cv2.resize(querydesc,0,fx=scale,fy=scale),queryKPs[m.queryIdx])-traindesc)**2)
-            #        for scale in scalerange]
+        x_tkp,y_tkp = tkp.pt
+        for i,scale in enumerate(scalerange):
+            r = qkp.size*scale*1.2*9/10
+            x0,y0 = roundtuple(x_tkp-r, y_tkp-r)
+            x1,y1 = roundtuple(x_tkp+r, y_tkp+r)
+            traintempl = trainImg[y0:y1+1, x0:x1+1,:]
+            dshape = traintempl.shape[1::-1] # reverse of the first 2 coords
+            scaledtempl = cv2.resize(querypatch,dshape
+                                     ,fx=scale,fy=scale, interpolation=cv2.INTER_LINEAR)
+            # skp = copy.copy(tkp)
+            # skp.pt = roundtuple(tkp.pt[0]-x0, tkp.pt[1]-y0)
+            # scaledesc = compute(scaledtempl,[skp])[1]
+            # traindesc = compute(traintempl,[skp])[1]
+            # res[i] = np.sum((scaledesc-traindesc)**2)/(scale**2)
+            res[i] = np.sum(scaledtempl-traintempl)/(scale**2)
 
-            x,y = trainKPs[m.trainIdx].pt
-            scale = scalerange[np.argmin(res)]
-            r *= scale
-            cv2.rectangle(trainTempImg, inttuple(x-r,y-r), inttuple(x+r,y+r), (255,0,0), 2)
-        except Exception, e:
-            print e
-            continue
+            cv2.imshow('Scaled', scaledtempl)
+            if cv2.waitKey(0)%256 == ord('q'): break
+                            
+        # res = [np.sum((compute(cv2.resize(querydesc,0,fx=scale,fy=scale),qkp)-traindesc)**2)
+        #        for scale in scalerange]
+
+        x,y = trainKPs[m.trainIdx].pt
+        scale = scalerange[np.argmin(res)]
+        r = qkp.size*scale*1.2*9/10
+        print "Match scale:", scale
+        cv2.rectangle(trainTempImg, roundtuple(x-r,y-r), roundtuple(x+r,y+r), (0,0,255), 2)
 
         cv2.imshow('Match', trainTempImg)
         if cv2.waitKey(0)%256 == ord('q'): return
 
-if __name__ == '__main__':
-    frmbuf = FrameBuffer(topic="/uvc_camera/image_raw")
 
-    # orb = cv2.ORB()
+if __name__ == '__main__':
+    # subscribe to the camera feed and grab the first frame
+    frmbuf = FrameBuffer(topic="/uvc_camera/image_raw")
+    lastFrame = frmbuf.grab()
+
+    # initialize the feature description and matching methods
     bfmatcher = cv2.BFMatcher()
     surf_ui = cv2.SURF(600)
 
-    # find the keypoints and descriptors with SIFT
-    lastFrame = frmbuf.grab()
+    # mask out a portion of the image
     roi = np.zeros(lastFrame.shape[:2],np.uint8)
-    scrapY, scrapX = lastFrame.shape[0]//16, lastFrame.shape[1]//16
-
+    scrapY, scrapX = lastFrame.shape[0]//8, lastFrame.shape[1]//8
     roi[scrapY:-scrapY, scrapX:-scrapX] = True
 
+    # get keypoints and feature descriptors from query image
+    qkp, qdesc = surf_ui.detectAndCompute(lastFrame,roi)
+    
     frameN = 1
-    kp1, desc1 = surf_ui.detectAndCompute(lastFrame,roi)
     while not rospy.is_shutdown():
         frameN += 1
-        currFrame = frmbuf.grab()      
-        kp2, desc2 = surf_ui.detectAndCompute(currFrame,roi)
+        currFrame = frmbuf.grab()
 
-        # matches = bfmatcher.match(desc1,desc2)
-        matches = bfmatcher.knnMatch(desc1,desc2,k=2)
+        # get keypoints and feature descriptors from training image
+        tkp, tdesc = surf_ui.detectAndCompute(currFrame,roi)
+
+        # find the best K matches between this and last frame
+        # matches = bfmatcher.match(qdesc,tdesc)
+        matches = bfmatcher.knnMatch(qdesc,tdesc,k=2)
         # matches.sort(key=attrgetter('distance'))
         # mindist = min(map(attrgetter('distance'),matches))
         # mindist = matches[0].distance
@@ -126,21 +146,21 @@ if __name__ == '__main__':
         # filter out poor matches by ratio test
         matches = [m[0] for m in matches if m and (m[0].distance < 0.7*m[1].distance)]
         # filter out features which haven't grown
-        matches = [m for m in matches if kp2[m.trainIdx].size > kp1[m.queryIdx].size]
+        matches = [m for m in matches if tkp[m.trainIdx].size > qkp[m.queryIdx].size]
         if not matches: continue
 
         # get only the keypoints that made a match
-        goodKPs = tuple( (kp1[m.queryIdx],kp2[m.trainIdx]) for m in matches )
+        goodKPs = tuple( (qkp[m.queryIdx],tkp[m.trainIdx]) for m in matches )
         mkp1, mkp2 = zip(*goodKPs)
-        dispim = cv2.drawKeypoints(currFrame,mkp1+mkp2, color=-1)
+        dispim = cv2.drawKeypoints(currFrame,mkp1+mkp2, None, color=-1)
                                    # , flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-        for i,m in enumerate(matches):
-            print "Match %3d: scale + %f" % (i,kp2[m.trainIdx].size-kp1[m.queryIdx].size)
+        # for i,m in enumerate(matches):
+        #     print "Match %3d: scale + %f" % (i,tkp[m.trainIdx].size-qkp[m.queryIdx].size)
 
-        scalings = findAppoximateScaling(lastFrame, currFrame, matches, desc1, desc2, kp1, kp2, surf_ui.compute)
+        scalings = findAppoximateScaling(lastFrame, currFrame, matches, qkp, tkp, surf_ui.compute)
         # draw the matches directly from the array of Dmatches
-        # map(lambda x: drawMatch(x,kp1,kp2,dispim), matches)
+        # map(lambda x: drawMatch(x,qkp,tkp,dispim), matches)
 
         # draw the accepted matches
         map(lambda p: cv2.line(dispim,tuple(map(int,p[0].pt))
@@ -148,13 +168,16 @@ if __name__ == '__main__':
             , goodKPs)
 
         cv2.imshow("Match", dispim)
-        if cv2.waitKey(1)%256 == ord('s'):
+        k = cv2.waitKey(1)%256
+        if k == ord('s'):
             fn1 = "Frame%d.png" % frameN-1
             fn2 = "Frame%d.png" % frameN
             print "saved as %s, %s..." % (fn1,fn2)
             cv2.imwrite(fn1,lastFrame)
             cv2.imwrite(fn2,currFrame)
+        elif k == ord('q'):
+            break
 
-        lastFrame, kp1, desc1 = currFrame, kp2, desc2
+        lastFrame, qkp, qdesc = currFrame, tkp, tdesc
 
     cv2.destroyAllWindows()
