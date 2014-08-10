ad99d47544f382ec76c66bdcd8966f792b874e9c
tdos <toemossgarcia@gmail.com>
2014-08-10 17:34:34 -0500

    flownav: lots of changes (too many really). Added extra keyboard controls for controlling drawing. Added terminal output for program launch. Added option for reading from a video file / camera.

--- scripts/flownav.py
+++ scripts/flownav.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python
 import rospy
-import roslib
-roslib.load_manifest('flownav')
+from std_srvs.srv import Empty
 
 import cv2
 import numpy as np
 import scipy.stats as stats
 
 from common import *
-from framebuffer import FrameBuffer
+from framebuffer import ROSCamBuffer,VideoBuffer
 from operator import attrgetter
-from drone_control import DroneController, DroneStatus
-from auto_control import AutoController,CharMap,KeyMapping
+from keyboard_control import KeyboardController,CharMap,KeyMapping
 
-from operator import itemgetter
-import itertools as it
-import time
-import sys
+import time,sys
 
+TEMPLATE_WIN = "Template matching"
+MAIN_WIN = "flownav"
 
-def estimateKeypointExpansion(queryImg, trainImg, matches, queryKPs, trainKPs, showMatches=False,dispimg=None,winname="Match"):
-    scalerange = 1 + np.arange(0.5+0.025,step=0.025)
+def estimateKeypointExpansion(queryImg, trainImg, matches, queryKPs, trainKPs, showMatches=False, dispimg=None):
+    scalerange = 1 + np.arange(0.6+0.025,step=0.025)
     res = np.zeros(len(scalerange))
     expandingKPs = []
+    kpscale = []
 
     for m in matches:
         qkp = queryKPs[m.queryIdx]
@@ -31,7 +29,6 @@ def estimateKeypointExpansion(queryImg, trainImg, matches, queryKPs, trainKPs, s
         # extract the query image patch
         x_qkp,y_qkp = qkp.pt
         r = qkp.size*1.2/9*20 // 2
-        # r = qkp.size//2
         x0,y0 = trunc_coords(queryImg.shape,(x_qkp-r, y_qkp-r))
         x1,y1 = trunc_coords(queryImg.shape,(x_qkp+r, y_qkp+r))
         querypatch = queryImg[y0:y1, x0:x1]
@@ -42,7 +39,6 @@ def estimateKeypointExpansion(queryImg, trainImg, matches, queryKPs, trainKPs, s
         res[:] = np.nan     # initialize all residuals as invalid
         for i,scale in enumerate(scalerange):
             r = qkp.size*scale*1.2/9*20 // 2
-            # r = qkp.size*scale // 2
             x0,y0 = trunc_coords(trainImg.shape,(x_tkp-r, y_tkp-r))
             x1,y1 = trunc_coords(trainImg.shape,(x_tkp+r, y_tkp+r))
             traintempl = trainImg[y0:y1, x0:x1]
@@ -65,9 +61,10 @@ def estimateKeypointExpansion(queryImg, trainImg, matches, queryKPs, trainKPs, s
         res_argmin = np.nanargmin(res)
         scalemin = scalerange[res_argmin]
         if scalemin > 1.2 and res[res_argmin] < 0.8*res[0]:
-            trainKPs[m.trainIdx].size = qkp.size*scalemin*1.2/9*20
-            # trainKPs[m.trainIdx].size = qkp.size*scalemin
+            # trainKPs[m.trainIdx].size = qkp.size*scalemin*1.2/9*20
+            trainKPs[m.trainIdx].size = qkp.size*scalemin
             expandingKPs.append(trainKPs[m.trainIdx])
+            kpscale.append(scalemin)
 
             if not showMatches: continue
 
@@ -81,18 +78,19 @@ def estimateKeypointExpansion(queryImg, trainImg, matches, queryKPs, trainKPs, s
                                      , interpolation=cv2.INTER_LINEAR)
 
             # draw the template and the best matching scaled version
-            templimg = np.zeros((scaledtempl.shape[0],scaledtempl.shape[1]+traintempl.shape[1]+querypatch.shape[1])
+            templimg = np.zeros((scaledtempl.shape[0]
+                                 ,scaledtempl.shape[1]+traintempl.shape[1]+querypatch.shape[1])
                                 , dtype=trainImg.dtype)
             templimg[:] = 255
 
             drawInto(querypatch, templimg)
             drawInto(scaledtempl,templimg,tl=(querypatch.shape[1],0))
-            drawInto(traintempl,templimg,tl=(querypatch.shape[1]+traintempl.shape[1],0))
+            drawInto(traintempl,templimg,tl=(querypatch.shape[1]+scaledtempl.shape[1],0))
             dispimg = cv2.drawKeypoints(dispimg,[trainKPs[m.trainIdx]], dispimg, color=(0,0,255)
                                         ,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
             
-            cv2.imshow('Template', templimg)
-            cv2.imshow(winname, dispimg)
+            cv2.imshow(TEMPLATE_WIN, templimg)
+            cv2.imshow(MAIN_WIN, dispimg)
             print "Relative scaling of template:",scalemin
             print "Find next match? ('s' to skip remaining matches,'q' to quit,enter to continue):",
             sys.stdout.flush()
@@ -104,7 +102,7 @@ def estimateKeypointExpansion(queryImg, trainImg, matches, queryKPs, trainKPs, s
             if k == ord('s'): showMatches=False
             elif k == ord('q'): raise SystemExit
 
-    return expandingKPs    
+    return expandingKPs, kpscale
 
 
 def generateuniqid():
@@ -124,7 +122,7 @@ from subprocess import Popen
 parser = optparse.OptionParser(usage="flownav.py [options]")
 parser.add_option("-b", "--bag", dest="bag", default=None
                   , help="Use feed from a ROS bagged recording.")
-parser.add_option("--threshold", dest="threshold", type=float,default=4000.
+parser.add_option("--threshold", dest="threshold", type=float, default=2500.
                   , help="Use feed from a ROS bagged recording.")
 parser.add_option("-m", "--draw-scale-match", dest="showmatches"
                   , action="store_true", default=False
@@ -132,20 +130,30 @@ parser.add_option("-m", "--draw-scale-match", dest="showmatches"
 parser.add_option("--no-draw", dest="nodraw"
                   , action="store_true", default=False
                   , help="Draw keypoints and their matches.")
-parser.add_option("--cam-topic", dest="camtopic", default="/ardrone"
-                  , help="Use another camera topic for testing purposes.")
-
+parser.add_option("--video-topic", dest="camtopic", default="/ardrone"
+                  , help="Specify the topic for camera feed (default='/ardrone').")
+parser.add_option("--video-file", dest="video", default=None
+                  , help="Load a video file to test or specify camera ID (typically 0).")
 (opts, args) = parser.parse_args()
 
 if opts.bag:
     DEVNULL = open(os.devnull, 'wb')
     bagp = Popen(["rosbag","play",opts.bag],stdout=DEVNULL,stderr=DEVNULL)
-
-# Initialize the drone controls and subscribe to the navdata feed
 rospy.init_node("flownav", anonymous=False)
-dronectrl = None if opts.camtopic != "/ardrone" else DroneController() 
-autoctrl = None if opts.camtopic != "/ardrone" else AutoController(dronectrl)
-frmbuf = FrameBuffer(opts.camtopic+"/image_raw")
+
+kbctrl = KeyboardController() if (opts.camtopic == "/ardrone" and not opts.video) else None
+if kbctrl:
+    FlatTrim = rospy.ServiceProxy("/ardrone/flattrim",Empty())
+    Calibrate = rospy.ServiceProxy("/ardrone/imu_recalib",Empty())
+else:
+    FlatTrim = lambda : None
+    Calibrate = lambda : None    
+
+if opts.video:
+    try: opts.video = int(opts.video)
+    except ValueError: pass
+    frmbuf = VideoBuffer(opts.video)
+else:           frmbuf = ROSCamBuffer(opts.camtopic+"/image_raw")
 
 # initialize the feature description and matching methods
 bfmatcher = cv2.BFMatcher()
@@ -163,21 +171,22 @@ getMatchKPs = lambda x: (queryKP[x.queryIdx],trainKP[x.trainIdx])
 # get keypoints and feature descriptors from query image
 queryKP, qdesc = surf_ui.detectAndCompute(lastFrame,roi)
 
-multimatches = {}
-
 idgen = generateuniqid()
 getuniqid = lambda : idgen.next()
+multimatches = {}
 
+#------------------------------------------------------------------------------#
 # Print intro output to user
+#------------------------------------------------------------------------------#
 print "Options"
 print "-"*len("Options")
-print "- Subscribed to", repr(opts.camtopic)
+print "- Subscribed to", (repr(opts.camtopic) if not opts.video else opts.video)
 print "- Hessian threshold set at", repr(opts.threshold)
 print
 
 print "-"*40
-cv2.namedWindow("Match", flags=cv2.WINDOW_OPENGL|cv2.WINDOW_NORMAL)
-if opts.showmatches: cv2.namedWindow("Template", flags=cv2.WINDOW_OPENGL|cv2.WINDOW_NORMAL)
+cv2.namedWindow(MAIN_WIN, flags=cv2.WINDOW_OPENGL|cv2.WINDOW_NORMAL)
+if opts.showmatches: cv2.namedWindow(TEMPLATE_WIN, flags=cv2.WINDOW_OPENGL|cv2.WINDOW_NORMAL)
 print "-"*40
 print
 
@@ -187,21 +196,20 @@ for k,v in CharMap.items():
     print k.ljust(20),'=',repr(v).ljust(5)
 print
 
-print
+print "Additional controls"
+print "-"*len("Additional controls")
 print "* Press 'q' at any time to quit"
 print "* Press 'd' at any time to toggle keypoint drawing"
 print "* Press 'm' at any time to toggle scale matching drawing"
-print
+print "* Press 'f' while drone is landed and level to perform a flat trim"
+print "* Press 'c' when drone is in a stable hover to recalibrate drone's IMU"
 
 #------------------------------------------------------------------------------#
 # main loop
 #------------------------------------------------------------------------------#
-frameN = 1
 while not rospy.is_shutdown():
     currFrame = frmbuf.grab()
     t1 = time.time()
-    frameN = frmbuf.msg.header.seq
-    frametime =  frmbuf.msg.header.stamp.to_sec()
 
     ### Find keypoint matches for this frame and filter them
     # get keypoints and feature descriptors from training image
@@ -246,18 +254,19 @@ while not rospy.is_shutdown():
         multimatches[clsid].age += 1
         if multimatches[clsid].age > MAX_AGE: del multimatches[clsid]
 
-    # get only the keypoints that made a match and are not expanding
-    matchKPs = tuple(getMatchKPs(m) for m in matches)
-                     # if multimatches[trainKP[m.trainIdx].class_id].detects > 3)
-
     ### Find expanding keypoints
+
+    # get all matching keypoints
+    matchKPs = [getMatchKPs(m) for m in matches
+                if multimatches[trainKP[m.trainIdx].class_id].detects > 1]
+    
     # filter out features which haven't grown
     matches = [m for m in matches
-               if trainKP[m.trainIdx].size > queryKP[m.queryIdx].size]
-               # and multimatches[trainKP[m.trainIdx].class_id].detects > 3]
+               if trainKP[m.trainIdx].size > queryKP[m.queryIdx].size
+               and multimatches[trainKP[m.trainIdx].class_id].detects > 1]
 
     ### Draw matches
-    dispim = dispim if not opts.nodraw else currFrame
+    dispim = None
     if not opts.nodraw:
         # draw each key point and a line between their match
         mkp1, mkp2 = zip(*matchKPs) if matchKPs else ([],[])
@@ -268,40 +277,50 @@ while not rospy.is_shutdown():
         cv2.rectangle(dispim,(scrapX,scrapY),(currFrame.shape[1]-scrapX,currFrame.shape[0]-scrapY)
                       ,(192,192,192),thickness=2)
 
-    expandingKPs = estimateKeypointExpansion(lastFrame, currFrame, matches
-                                             , queryKP, trainKP
-                                             , showMatches=opts.showmatches
-                                             , dispimg=dispim
-                                             , winname="Match")
-    print time.time() - t1
+    expandingKPs, kpscales = estimateKeypointExpansion(lastFrame, currFrame, matches
+                                                       , queryKP, trainKP
+                                                       , showMatches=opts.showmatches
+                                                       , dispimg=dispim)
 
-    # draw each expanding key point
-    if not opts.showmatches:
-        cv2.drawKeypoints(dispim,expandingKPs, dispim, color=(0,0,255)
-                          ,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    sys.stdout.write("Loop time: %3.3f ms\r" % (1000.*(time.time() - t1))); sys.stdout.flush()
 
+    if not opts.showmatches: # draw each expanding key point
+        dispim = cv2.drawKeypoints(dispim if dispim is not None else currFrame
+                                   , expandingKPs, dispim, color=(0,0,255)
+                                   , flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     
-    if dronectrl:
-        vx, vy, vz = inttuple(*attrgetter('vx','vy','vz')(dronectrl.navdata))
-        batt = dronectrl.navdata.batteryPercent
+    if kbctrl:
+        vx, vy, vz = inttuple(*attrgetter('vx','vy','vz')(kbctrl.navdata))
+        batt = kbctrl.navdata.batteryPercent
         stat = "BATT=%.2f" % (batt)
-        cv2.putText(dispim,stat,(10,currFrame.shape[0]-20)
+        cv2.putText(dispim,stat,(10,currFrame.shape[0]-10)
                     ,cv2.FONT_HERSHEY_TRIPLEX, 0.65, (0,0,255))
-    cv2.imshow("Match", dispim)
 
-    indanger = False
-    x,y = avgKP(expandingKPs) if expandingKPs else 0,0
-    if indanger:
-        if x < currFrame.shape[1]:  dronectrl.EvadeRight()
-        else:                       dronectrl.EvadeLeft()
+    x_obs, y = avgKP(expandingKPs) if expandingKPs else (currFrame.shape[1]//2,currFrame.shape[0]//2)
+    cv2.line(dispim, (int(x_obs),scrapY+2), (int(x_obs),currFrame.shape[0]//2-20), (0,255,0), 1)
+    cv2.line(dispim, (int(x_obs),currFrame.shape[0]//2+20), (int(x_obs),currFrame.shape[0]-scrapY-2), (0,255,0), 1) 
 
+    cv2.imshow(MAIN_WIN, dispim)
     k = cv2.waitKey(1)%256
-    if autoctrl and k in KeyMapping:    autoctrl.keyPressEvent(k)
-    elif k == ord('m'):                 opts.showmatches ^= True
-    elif k == ord('d'):                 opts.nodraw ^= True
-    elif k == ord('q'):                 break
+    if kbctrl:          kbctrl.keyPressEvent(k)
+    if kbctrl:          kbctrl.React(expandingKPs,currFrame)
+    if k == ord('m'):
+        opts.showmatches ^= True
+        if opts.showmatches: cv2.namedWindow(TEMPLATE_WIN, flags=cv2.WINDOW_OPENGL|cv2.WINDOW_NORMAL)
+        else: cv2.destroyWindow(TEMPLATE_WIN)
+    elif k == ord('d'):
+        opts.nodraw ^= True
+    elif k == ord('f'):
+        try: FlatTrim()
+        except rospy.ServiceException, e: print e
+    elif k == ord('c'):
+        try: Calibrate()
+        except rospy.ServiceException, e: print e
+    elif k == ord('q'):
+        break
 
     lastFrame, queryKP, qdesc = currFrame, trainKP, tdesc
 
 if opts.bag: bagp.kill()
 cv2.destroyAllWindows()
+frmbuf.close()
