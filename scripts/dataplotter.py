0d6d7e93ff8e05a0cc574b71a7cfd6d0ae73f0ba
tdos <toemossgarcia@gmail.com>
2014-12-11 00:03:54 -0600

    dataplotter: separate keypoint plotting. buggy legend/colors.

--- scripts/dataplotter.py
+++ scripts/dataplotter.py
@@ -6,45 +6,13 @@ from flownav.msg import keypoint as kpMsg
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
-from matplotlib import animation
+import itertools
+import random
 
 BUFSIZE = 160
-SCROLLSIZE = 15
+SCROLLSIZE = 80
 NTRACKEDKPS = 10
-AUTOSCALE = True
-
-buffer_dtype = [('size',np.uint32,(NTRACKEDKPS,)), ('id',np.uint32,(NTRACKEDKPS,))]
-databuffer = np.zeros((BUFSIZE,),dtype=buffer_dtype)
-frames = np.arange(BUFSIZE,dtype=np.uint64)
-databuffer.fill(np.nan)
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.set_ylabel("Relative scale"); ax.set_xlabel("Frame")
-# ax.set_xlim(1,BUFSIZE)
-# ax.set_ylim(0.9,2)
-ax.grid()
-fig.tight_layout()
-
-markers = []
-for m in plt.Line2D.markers:
-    try:
-        if len(m) == 1 and m != ' ':
-            markers.append(m)
-    except TypeError:
-        pass
-colors = ('b', 'g', 'r', 'c', 'm', 'y', 'k')
-
-lines = [plt.Line2D((),(),marker=markers[i],color=colors[i%len(colors)])
-         for i in range(databuffer['size'].shape[1])]
-lines[0].set_linewidth(2)
-lines[0].set_alpha(0.6)
-
-legend = plt.legend([plt.Line2D([],[], linewidth=5, color=c) for c in colors]
-                    , [], prop={'size':'medium'}, frameon=False,ncol=len(colors)
-                    , bbox_to_anchor=(0, 1.015, 1., .05), mode='expand')
-ax.add_artist(legend)
-
+NMAXLINES = 100
 
 class Storage:
     def __init__(self, a, path, chunkSize=10):
@@ -68,111 +36,158 @@ class Storage:
             self._buffer.tofile(storagefile)
 
 
-class Plotter:
-    def __init__(self, a, storeData=False, dataPath='arr.npy', scrollSize=SCROLLSIZE):
-        self.__index = 0
-        self.__startFrame = 0
+class DataSubscriber(object):
+    def __init__(self, a, storeData=False, dataPath='arr.npy', scrollsize=SCROLLSIZE):
         self.storage = Storage(a, dataPath) if storeData else None
+        self.buffersize = len(a)
+        self.scrollsize = scrollsize
+        self.returnindex = self.buffersize - self.scrollsize
+        self.lines = self.lastlines = []
+
         self._buffer = a
-        self.bufSize = len(a)
-        self._indexBuffer = np.arange(len(a))
-        self.scrollSize = scrollSize
+        self._range = np.arange(len(a))
+
+        self.__index = 0
+        self.__bufferindex = 0
+
+        self.subscriber = self.connect()
+
+    def connect(self):
+        print "Attempting to connect to publisher..."
+
+        rospy.init_node('ttcplotter')
+
+        try:
+            s = rospy.topics.Subscriber("/flownav/data", ttcMsg, self)
+            while not rospy.core.is_shutdown() and self.__index == 0:
+                rospy.rostime.wallsleep(0.01)
+            if not rospy.core.is_shutdown(): print "Connected."
+        except ROSInterruptException:
+            s.unregister()
+            raise
+
+        return s
 
     @property
     def index(self): return self.__index
         
     def roll(self):
-        global frames
-
         if self.storage is not None:
-            self.storage.store(self._buffer[:self.scrollSize])
+            self.storage.store(self._buffer[:self.scrollsize])
 
-        frames += self.scrollSize
-        self._buffer[:-self.scrollSize] = self._buffer[self.scrollSize:]
-        self._buffer[-self.scrollSize:] = np.nan        
-        self.__index = self.bufSize-self.scrollSize-1
+        self._range += self.scrollsize
+        self._buffer[:-self.scrollsize] = self._buffer[self.scrollsize:]
+        self._buffer[-self.scrollsize:] = np.nan        
+
+        self.__bufferindex = self.returnindex-1
+
+    def __enter__(self):
+        return self
 
     def __call__(self, datum):
-        objSize = self._buffer['size']
-
-        if self.__index > 0 and self.__index == self.bufSize:
-            self.roll()
-
-            global AUTOSCALE, frames
-            if AUTOSCALE and np.nanmin(objSize) != np.nanmax(objSize):
-                ax.set_xlim(frames[0],frames[0]+self.bufSize-self.scrollSize)
-                ax.set_ylim(np.nanmin(objSize),np.nanmax(objSize))
-                ax.autoscale_view(True,True,True)
-
-        keypoints = datum.keypoints
-        kpSizes = [keypoints[i].trainSize for i in range(min(NTRACKEDKPS,len(keypoints)))]
-                   # for i in range(min(objSize.shape[0]-1,len(datum.keypoints)))]
-        ids = [keypoints[i].class_id for i in range(min(NTRACKEDKPS,len(keypoints)))]
-
-        if len(kpSizes) > 0:
-            self._buffer['id'][self.__index, :len(kpSizes)] = ids
-            objSize[self.__index, :len(kpSizes)] = kpSizes
-            # objSize[self.__index, 0] = np.mean(kpSizes)
-            # objSize[self.__index, 1:len(kpSizes)+1] = kpSizes
-        
         self.__index += 1
+        if self.__bufferindex == self.buffersize: self.roll()
+
+        obj_size = self._buffer['size']
+        obj_id = self._buffer['id']
+
+        for l in set(self.lastlines).difference(datum.keypoints[:NTRACKEDKPS]):
+            self.lines.remove(l)
+
+        for kp in datum.keypoints[:NTRACKEDKPS]:
+            clsid = kp.class_id
+
+            if clsid in self.lines:
+                i = self.lines.index(clsid)
+            else:
+                i = len(self.lines)
+                self.lines.append(clsid)
+
+            obj_id[self.__bufferindex, i] = clsid
+            obj_size[self.__bufferindex, i] = kp.trainSize
+            
+        # objSize[self.__bufferindex, 0] = np.mean(kpSizes)
+        # objSize[self.__bufferindex, 1:len(kpSizes)+1] = kpSizes
+        
+        self.lastlines = self.lines
+        self.__bufferindex += 1
+
+    def __exit__(self, exception_type, exception_val, trace):
+        self.close()
+        if exception_type is not None: raise
+        return True
 
     def close(self):
-        if self.storage is not None:
-            self.storage.close()
+        if self.storage is not None: self.storage.close()
+        self.subscriber.unregister()
+        return True
+
         
+class DataPlotter(DataSubscriber):
+    markers = []
+    for m in plt.Line2D.markers:
+        try:
+            if len(m) == 1 and m != ' ':
+                markers.append(m)
+        except TypeError:
+            pass
+    colors = ('b', 'g', 'r', 'c', 'm', 'y', 'k')
 
-def init():
-    global legend, lines, ax
-    for graph in lines:
-        ax.add_line(graph)
-    ax.hold(False)
-    return lines, legend
+    def __init__(self,a,**kwargs):
+        super(self.__class__, self).__init__(a,**kwargs)
 
-def animate(frameIdx):
-    global lines, legend, databuffer
-    mask = ~np.isnan(databuffer['size'][:-SCROLLSIZE,0])
+        plt.ion()
+        self.fig = plt.figure(figsize=(10.24,7.68),dpi=100,frameon=False)
+        ax = self.fig.add_subplot(111)
+        ax.set_ylabel("Patch size"); ax.set_xlabel("Frame")
+        ax.grid()
+        self.fig.tight_layout()
+        self.combos = list(itertools.product(self.markers,self.colors))
 
-    for i,graph in enumerate(lines):
-        data = databuffer['size'][mask,i]
-        if not data.size:
-            ax.lines.remove(graph)
-            continue
-        if graph not in ax.lines:
-            ax.add_line(graph)
-        graph.set_data(frames[mask], data)
+    def add_line(self,**kwargs):
+        marker, color = self.combos[random.randint(0,len(self.combos)-1)]
+        ax = self.fig.axes[0]
+        if len(ax.lines) == NMAXLINES: ax.lines[0].remove()
 
-    return lines, legend, ax
+        line = plt.Line2D((),(),marker=marker,color=color,**kwargs)
+        return self.fig.axes[0].add_line(line)
 
-def listener(callback):
-    # in ROS, nodes are unique named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The 
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaenously.
-    print "Attempting to connect to publisher..."
+    def update_plot(self):
+        ax = self.fig.axes[0]
+        obj_size = self._buffer['size']
+        obj_id = self._buffer['id']
+        lines = self.lines
+        if not lines: return
+        
+        axlabels = [line.get_label() for line in ax.lines]
+        for i,line_id in enumerate(lines):
+            if line_id not in axlabels:
+                line = self.add_line(label=line_id)
+            else:
+                line = ax.lines[axlabels.index(line_id)]
 
-    rospy.init_node('ttcplotter')
-    s = rospy.topics.Subscriber("/flownav/data", ttcMsg, callback)
-    while not rospy.core.is_shutdown() and callback.index == 0:
-        rospy.rostime.wallsleep(0.01)
-    if not rospy.core.is_shutdown(): print "Connected."
-    else:
-        s.unregister()
-        raise rospy.exceptions.ROSInterruptException
+            mask = obj_id[:,i] == line_id
+            line.set_data(self._range[mask], obj_size[mask,i])
 
-    return s
+        ax.relim()
+        ax.autoscale_view(scaley=True)
+        ax.set_xlim(self._range[0],self._range[-1])
+
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend(labels[-len(self.lines):])
+        self.fig.set_size_inches(*self.fig.get_size_inches())
+        self.fig.canvas.draw()
+
 
 if __name__ == '__main__':
-    p = Plotter(databuffer,storeData=False, dataPath='../data/arr.npy')
+    buffer_dtype = [('size',np.uint32,(NTRACKEDKPS,)), ('id',np.uint32,(NTRACKEDKPS,))]
+    databuffer = np.zeros((BUFSIZE,),dtype=buffer_dtype)
+    databuffer.fill(np.nan)
 
-    try:
-        sub = listener(p)
-    except rospy.exceptions.ROSInterruptException:
-        p.close()
-        sys.exit(1)
+    dataPath = sys.argv[1] if len(sys.argv)>1 else '../data/arr.npy'
+    storeData = len(sys.argv)>1
 
-    anim = animation.FuncAnimation(fig, animate, init_func=init, blit=False, interval=100)
-    plt.show()
-    p.close()
-    sub.unregister()
+    with DataPlotter(databuffer,storeData=storeData,dataPath=dataPath) as p:
+        while not rospy.core.is_shutdown():
+            p.update_plot()
+            rospy.rostime.wallsleep(0.1)
